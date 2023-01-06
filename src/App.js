0225de403d885af989d594e7c441139af8eab33f
Yulia Gutsal <julik.ua@gmail.com>
2023-01-06 16:51:46 +0200

    create useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Clock from './components/Clock';
+import './components/Clock.css';
+import Count from './components/Count';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Clock/>
+      <br/>
+      <Count/>
     </div>
   );
 }
---
+++ src/components/FriendStatus.js
@@ -0,0 +1,7 @@
+import React, {useState, useeffect} from 'react'
+
+export default function FriendStatus() {
+  return (
+    <div>FriendStatus</div>
+  )
+}
